afd865b43175a0bbb989298929c5b3c2039a2bc4
glashenka20 <glafiratimchenko@gmail.com>
2024-11-06 15:44:25 +1000

    fgjfgj

--- WpfExamDemoZZZ/MainWindow.xaml.cs
+++ WpfExamDemoZZZ/MainWindow.xaml.cs
@@ -22,28 +22,41 @@ namespace WpfExamDemoZZZ
         public User User { get; set; } = new User();
         public List<User> Users { get; set; } = new List<User>();
         public List<Role> Roles { get; set; } = new List<Role>();
+        public int ErrorCount { get; set; }
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
             client.BaseAddress = new Uri("http://localhost:5185/api/");
+            ErrorCount = 0;
         }
 
-        private async Task SignInClick(object sender, RoutedEventArgs e)
+        private async void SignInClick(object sender, RoutedEventArgs e)
+        {
+            await NewMethodNew();
+        }
+
+        private async Task NewMethodNew()
         {
             User.Password = password.Password;
 
-            var responce = await client.PostAsync("User/CheckUserInDB", new StringContent("{}", Encoding.UTF8, "application/json"));
-            var responce2 = await client.PostAsync("User/CheckUserRole", new StringContent("{}", Encoding.UTF8, "application/json"));
-            var responce3 = await client.PostAsync("User/CheckFirstSign", new StringContent("{}", Encoding.UTF8, "application/json"));
+            var responce = await client.PostAsync($"User/CheckUserInDB?login={User.Login}&password={User.Password}", 
+                new StringContent("{}", Encoding.UTF8, "application/json"));
+            var responce2 = await client.PostAsync("User/CheckUserRole", new StringContent($"{User.Login}, {User.Password}", Encoding.UTF8, "application/json"));
+            var responce3 = await client.PostAsync("User/CheckFirstSign", new StringContent($"{User.Login}, {User.Password}", Encoding.UTF8, "application/json"));
             var responce4 = await client.PostAsync("User/CheckUserIsBlocked", new StringContent($"{User.Login}, {User.Password}", Encoding.UTF8, "application/json"));
+
             //Users = await responce.Content.ReadFromJsonAsync<List<User>>();
             //Roles = await responce2.Content.ReadFromJsonAsync<List<Role>>();
             //var user = Users.FirstOrDefault(s => s.Login == User.Login && s.Password == User.Password);
+            var shit = await responce.Content.ReadAsStringAsync();
             var answerCheckUserInDB = await responce.Content.ReadFromJsonAsync<bool>();
+
             var answerCheckUserRole = await responce2.Content.ReadFromJsonAsync<bool>();
             var answerCheckFirstSign = await responce3.Content.ReadFromJsonAsync<bool>();
             var answerCheckUserIsBlocked = await responce4.Content.ReadFromJsonAsync<bool>();
+
+            //var a = await client.GetFromJsonAsync<bool>("User/CheckUserIsBlocked");
 
             if (answerCheckUserIsBlocked)
             {
@@ -72,7 +85,7 @@ namespace WpfExamDemoZZZ
                         }
                     }
                 }
-                else MessageBox.Show("Вы вввели неверный логин или пароль. Пожалуйста проверьте ещё раз введенные данные");
+                else { MessageBox.Show("Вы вввели неверный логин или пароль. Пожалуйста проверьте ещё раз введенные данные"); ErrorCount++; }
             }
             else MessageBox.Show("Вы заблокированы. Обратитесь к администратору");
         }
